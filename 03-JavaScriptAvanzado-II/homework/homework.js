b1dd349d25834d4d566871c2aa39ddd00dd54c06
Pablo Alastra <alastra.pablo@gmail.com>
2023-03-22 18:03:42 -0300

    03 done

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -88,16 +88,20 @@ let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
-  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación,
+  tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones
+  bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto
+  - ya que los otros argumentos habrán sido "bindeados". 
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos = crearCadena.bind();
-let textoGuiones = crearCadena.bind();
-let textoUnderscore = crearCadena.bind();
+let textoAsteriscos = crearCadena.bind(this, "*", "*");
+let textoGuiones = crearCadena.bind(this, "-", "-");
+let textoUnderscore = crearCadena.bind(this, "_", "_");
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
