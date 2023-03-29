7e7d193813e273f662c944b7f105e4cabfda5967
Pablo Alastra <alastra.pablo@gmail.com>
2023-03-29 11:14:00 -0300

    fibo & fact

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,35 +1,69 @@
 "use strict";
 
-/*
-Definir las funciones recursivas nFactorial y nFibonacci.
 
-nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial
+
+/* Definir las funciones recursivas nFactorial y nFibonacci.
+ 
+   Como ejercicio adicional y completamente opcional, al terminar
+    de resolver este problema pueden intentar definir funciones
+    que logren los mismos resultados pero de manera iterativa.
+*/
+
+/* nFactorial(n)
+debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial
 (representado como n!) es el producto de n por todos los números naturales menores que él y mayores
 a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
+*/
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1,
+function nFactorial(n) {
+  if (n <= 1) {
+    return 1
+  }
+  return n * nFactorial(n - 1)
+}
+
+console.log(nFactorial(4));
+
+function nFactorialIter(n) {
+  let factorial = 1
+
+  for (let i = n; i > 1; i--) {
+    factorial *= i
+  }
+
+  return factorial
+}
+
+console.log(nFactorialIter(4));
+
+/* nFibonacci(n)
+ debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1,
  respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que 
  se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
-Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
-
-
-Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar 
-definir funciones que logren los mismos resultados pero de manera iterativa.
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 */
 
-function nFactorial(n) {
-  if (Number.isInteger(n) && n === 0) {
-    return 0;
+function nFibonacci(n) {
+  if (n < 2) {
+    return n
   }
-  n *= n - 1;
-  n--;
-  return nFactorial(n);
+  return nFibonacci(n - 1) + nFibonacci(n - 2)
 }
-console.log(nFactorial(2));
 
-function nFibonacci(n) {}
+console.log(nFibonacci(8));
+
+function nFibonacciIter(n) {
+  let fibonacci = [0, 1]
+
+  for (let i = 2; i <= n; i++) {
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2])
+  }
+  return fibonacci[fibonacci.length - 1]
+}
+
+console.log(nFibonacciIter(8));
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -40,7 +74,7 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() { }
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
