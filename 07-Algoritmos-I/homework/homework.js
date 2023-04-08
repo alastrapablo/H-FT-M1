45be38ea43bf3ff4ba259ef89daba8958cb15db6
Pablo Alastra <alastra.pablo@gmail.com>
2023-04-08 13:51:27 -0300

    07 done

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -32,21 +32,21 @@ Devolver el array ordenado resultante
 //1
 function bubbleSort(array) {
   let arr = array
-  let lengthArr = array.length
+  let lengthArr = arr.length
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < lengthArr; j++) {
       if (arr[j] > arr[j + 1]) {
-        var n = arr[j + 1]
+        let temp = arr[j + 1]
         arr[j + 1] = arr[j]
-        arr[j] = n
+        arr[j] = temp
       }
     }
-    lengthArr - 1
+    lengthArr--
   }
   return arr
 }
 
-console.log(bubbleSort(2, 5, 1, 8, 4, 22[, 3]));
+console.log(bubbleSort([2, 5, 1, 8, -7, 4, 22, 3]));
 
 //2
 function bubbleSort2(arr) {
@@ -68,24 +68,75 @@ function bubbleSort2(arr) {
 console.log(bubbleSort2([8, 20, -2, 4, -6, 5, 1]))
 
 //* INSERTION SORT
+//Big-O = O(n ** 2)
 /* Implementar el método conocido como insertionSort para ordenar de menor a mayor
 el array recibido como parámetro utilizando arreglos
 Devolver el array ordenado resultante */
 
 function insertionSort(array) {
-
+  let arr = array;
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
 }
 
+console.log(insertionSort([2, 5, 1, 8, -7, 4, 22, 3]));
+
 //* SELECTION SORT
+//Big-O = O(n * m)
 /* Implementar el método conocido como selectionSort para ordenar de menor a mayor
   el array recibido como parámetro utilizando dos arreglos
   Devolver el array ordenado resultante
  */
 
+//1
 function selectionSort(array) {
+  let arr = array;
+  let arrPosition = 0
 
+  for (let i = 0; i < arr.length; i++) {
+    let n = arr[i]
+    let m = Infinity
+    let mPosition
+    for (let j = arrPosition; j < arr.length; j++) {
+      if (arr[j] <= n && arr[j] < m) {
+        m = arr[j]
+        mPosition = j
+      }
+    }
+    arr[i] = arr[mPosition]
+    arr[mPosition] = n
+    arrPosition++
+  }
+  return arr
 }
 
+console.log(selectionSort([2, 5, 1, 8, -7, 4, 22, 3, 1]));
+
+//2
+function selectionSort2(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    // Intercambiar el valor mínimo con el primer elemento de la sublista no ordenada
+    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+  }
+  return arr;
+}
+
+console.log(selectionSort2([2, 5, 1, 8, -7, 4, 22, 3, 1]));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
