d7286207cdf200e4bdcc38923078fa36cb6d7de6
Pablo Alastra <alastra.pablo@gmail.com>
2023-04-08 11:48:26 -0300

    07 bubble

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,11 +1,13 @@
 'use strict'
-// No cambies los nombres de las funciones.
+
+//* FACTOREAR
+/*
+ Factorear el número recibido como parámetro y devolver en un array los factores por los cuales
+ se va dividiendo a dicho número (De menor a mayor).
+ Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
+*/
 
 function factorear(num) {
-  // Factorear el número recibido como parámetro y devolver en un array
-  // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
-  // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
-  // Tu código:
   let arr = [1];
   let divisor = 2;
   while (num > 1) {
@@ -18,32 +20,69 @@ function factorear(num) {
   return arr;
 }
 
-
 console.log(factorear(180));
+console.log(factorear(45));
 
+//* BUBBLE SORT
+/* 
+Implementar el método conocido como bubbleSort para ordenar de menor a mayorel array recibido como parámetro 
+Devolver el array ordenado resultante
+*/
+
+//1
 function bubbleSort(array) {
-  // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
-  // el array recibido como parámetro
-  // Devolver el array ordenado resultante
-  // Tu código:
-
+  let arr = array
+  let lengthArr = array.length
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < lengthArr; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var n = arr[j + 1]
+        arr[j + 1] = arr[j]
+        arr[j] = n
+      }
+    }
+    lengthArr - 1
+  }
+  return arr
 }
 
+console.log(bubbleSort(2, 5, 1, 8, 4, 22[, 3]));
+
+//2
+function bubbleSort2(arr) {
+  let swapped
+  do {
+    swapped = false
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+        swapped = true
+      }
+    }
+  } while (swapped)
+  return arr
+}
+
+console.log(bubbleSort2([8, 20, -2, 4, -6, 5, 1]))
+
+//* INSERTION SORT
+/* Implementar el método conocido como insertionSort para ordenar de menor a mayor
+el array recibido como parámetro utilizando arreglos
+Devolver el array ordenado resultante */
 
 function insertionSort(array) {
-  // Implementar el método conocido como insertionSort para ordenar de menor a mayor
-  // el array recibido como parámetro utilizando arreglos
-  // Devolver el array ordenado resultante
-  // Tu código:
 
 }
 
+//* SELECTION SORT
+/* Implementar el método conocido como selectionSort para ordenar de menor a mayor
+  el array recibido como parámetro utilizando dos arreglos
+  Devolver el array ordenado resultante
+ */
 
 function selectionSort(array) {
-  // Implementar el método conocido como selectionSort para ordenar de menor a mayor
-  // el array recibido como parámetro utilizando dos arreglos
-  // Devolver el array ordenado resultante
-  // Tu código:
 
 }
 
